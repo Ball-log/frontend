81b8988fecb8cbe6a032698755427d607e916cde
bandalgomim <bandalgomim@naver.com>
2024-09-03 21:44:09 +0900

    복원지점

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { community_context_api } from '../api/community/community.context'
+import { community_api } from '../api/community/community.api';
+import { community_error } from '../api/community/community.error';
+import { comment_api } from '../api/api-utils/comment.api';
+import { reply_api } from '../api/api-utils/reply.api';
 
 const Context = React.createContext();
 const ContextProvider = ({children}) => {
@@ -9,9 +12,98 @@ const ContextProvider = ({children}) => {
       })
     const [postList, setPostList] = useState([{data: {}}]);
     
-    const community_context = community_context_api
+    const community_context = {
+        get: async (post_id) => {
+            try {
+                const response = await community_api.get(post_id);
+                setPostData(response.result);
+            } catch (error) {
+                community_error.get(error);
+            }
+        },
+        post: async (req) => {
+            try {
+                await community_api.post(req);
+            } catch (error) {
+                community_error.post(error);
+            }
+        },
+        patch: async (post_id, req) => {
+            try {
+                await community_api.patch(post_id, req);
+            } catch (error) {
+                community_error.patch(error);
+            }
+        },
+        delete: async (post_id) => {
+            try {
+                await community_api.delete(post_id);
+            } catch (error) {
+                community_error.delete(error);
+            }
+        },
+        get_list: async (type, cursor = null, page = null) => {
+            try {
+                const response = await community_api.get_list(type, cursor, page);
+
+                if (response.result.totalCount  === 0) {
+                    setPostList([{data: []}])
+                } else {
+                    setPostList(response.result);
+                }
+            } catch (error) {
+                community_error.get_list(error);
+            }
+        }
+    };
+    const comment_context = {
+        post: async (req) => {
+            try {
+                await comment_api.post(req);
+            } catch (error) {
+                //comment_error.post(error);
+            }
+        },
+        patch: async (req) => {
+            try {
+                await reply_api.patch(req);
+            } catch (error) {
+                //comment_error.patch(error);
+            }
+        },
+        delete: async (req) => {
+            try {
+                await comment_api.delete(req);
+            } catch (error) {
+                //comment_error.delete(error);
+            }
+        }
+    }
+    const reply_context = {
+        post: async (req) => {
+            try {
+                await reply_api.post(req);
+            } catch (error) {
+                //reply_error.post(error);
+            }
+        },
+        patch: async (req) => {
+            try {
+                await reply_api.patch(req);
+            } catch (error) {
+                //reply_error.patch(error);
+            }
+        },
+        delete: async (req) => {
+            try {
+                await reply_api.delete(req);
+            } catch (error) {
+                //reply_error.delete(error);
+            }
+        }
+    }
     return (
-        <Context.Provider value={{ community_context, postData, postList }}>
+        <Context.Provider value={{ community_context, reply_context, comment_context, postData, postList }}>
             {children}
         </Context.Provider>
     );
