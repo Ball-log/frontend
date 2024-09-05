2c1efc6e5c19914314002cdb06c36b92fe936a42
bandalgomim <bandalgomim@naver.com>
2024-09-05 20:54:56 +0900

    Fix: 오류

--- src/screens/community/ComuPostedScreen.js
+++ src/screens/community/ComuPostedScreen.js
@@ -172,7 +172,7 @@ export default function ComuPostedScreen() {
           <LayoutBox>
           
         </LayoutBox>
-        {/*<CommentBody/>*/}
+        <CommentBody/>
       </ScrollContainer>
       
     </Wrapper>
